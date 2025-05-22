42aea3bdd8fadf637beb295c0818106a867f5f6e
ihsanakbay <ihsanakbay0@gmail.com>
2025-05-22 10:39:34 +0300

    Initial Commit

---
+++ UIKitBaseMVVMArch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitBaseMVVMArch
+//
+//  Created by İhsan Akbay on 22.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
